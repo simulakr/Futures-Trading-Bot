431a96d4846ab28f75213e1adb5e6ec7d46ab046
Yunus Bilgiç <72708117+simulakr@users.noreply.github.com>
2026-04-20 11:36:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ SYMBOLS  = [ "ETHUSDC", "SOLUSDT", "XRPUSDT", "DOGEUSDT"] #"BTCUSDT","BNBUSDT"
 INTERVAL = "15m"
 
 # ─── Percent ATR Filtreleri ───────────────────────────────────────────────────
-atr_ranges = {
+ATR_RANGES = {
     "BTCUSDT":  (0.173, 0.645),
     "ETHUSDC":  (0.363, 0.990),
     "SOLUSDT":  (0.423, 1.176),
